2e18b57d12eac4791bd858969c051aa84f0e7071
Kachel Wittig <kjwittig@gmail.com>
2018-03-28 16:13:48 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    sentences = self.split("." || "!" || "?")
+    sentences = self.split("." || "!" || "?" || "...")
     if sentences.empty?
       return 0
     else
